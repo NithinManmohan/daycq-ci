7651910ed1152f9dd66d0f26ddcf7f43bb031ec0
Nithin Manmohan <nmanmohan@sapient.com>
2013-05-07 11:21:27 +0530

    added TEMPLATES folder

---
+++ TEMPLATES/setenv.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# ENVARS for Tomcat and TDS environment
+#
+CATALINA_HOME="/apps/ci/apache-tomcat-7.0.39"
+export CATALINA_HOME
+
+JAVA_OPTS="-Xmx1024m -Xms512m -server -Djava.awt.headless=true -Djava.util.prefs.systemRoot=$CATALINA_HOME/content/thredds/javaUtilPrefs"
+export JAVA_OPTS
