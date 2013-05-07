d63493e9ae411492f6ab038574790aaa94c5976e
Nithin Manmohan <nmanmohan@sapient.com>
2013-05-07 10:57:09 +0530

    modified prebuild.sh script

--- prebuild.sh
+++ prebuild.sh
@@ -1,2 +1,2 @@
 TOMCAT=apache-tomcat-7.0.39
-curl -L http://mirror.nexcess.net/apache/tomcat/tomcat-7/v7.0.39/bin/${TOMCAT}.tar.gz
+curl -L http://mirror.nexcess.net/apache/tomcat/tomcat-7/v7.0.39/bin/${TOMCAT}.tar.gz -o BUILD/
