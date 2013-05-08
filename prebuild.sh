8e32b75c00e33b60786f69c55150f44f34775077
Nithin Manmohan <nmanmohan@sapient.com>
2013-05-08 10:16:07 +0530

    modified prebuild script

--- prebuild.sh
+++ prebuild.sh
@@ -1,6 +1,10 @@
 TOMCAT=apache-tomcat-7.0.39
 SONAR=sonar-3.5.1
 NEXUS=nexus-2.3.1
+rm -rf BUILD/$TOMCAT.tar.gz
+rm -rf BUILD/$SONAR.zip
+rm -rf BUILD/jenkins.war
+rm -rf BUILD/$NEXUS.war
 curl -L http://mirror.nexcess.net/apache/tomcat/tomcat-7/v7.0.39/bin/${TOMCAT}.tar.gz -o BUILD/${TOMCAT}.tar.gz
 curl -L http://mirrors.jenkins-ci.org/war/latest/jenkins.war -o BUILD/jenkins.war
 curl -L http://dist.sonar.codehaus.org/${SONAR}.zip -o BUILD/${SONAR}.zip
